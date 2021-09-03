8511af15249fc1c22edb8b6f548ae0e5e2b7395b
mustafa.ay <aymustafa.32@gmail.com>
2021-09-03 23:19:21 +0300

    Arrow Fest || Bazı Temel Mekanikler Eklendi

---
+++ Assets/Scripts/ArrowMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int zForce = 2;
+    public float horizontalForce = .001f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, zForce * Time.deltaTime);
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-horizontalForce * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("d"))
+        {
+            rb.AddForce(horizontalForce * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            rb.AddForce(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour
+{
+    public ArrowMovement movement;
+
+    private void OnCollisionEnter(Collision hitObstacle)
+    {
+        if(hitObstacle.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
